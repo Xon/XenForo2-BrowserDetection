e08a69b4f0118110c4904a92f28f1945a9f3a0ed
Xon <tazipper1@hotmail.com>
2019-06-14 05:01:22 +0800

    XF2.1+ Full page caching integration

--- README.md
+++ README.md
@@ -2,5 +2,19 @@
 
 A light-weight shim around [Mobile_detect](https://github.com/serbanghita/Mobile-Detect) for XenForo 2
 
+
+## XF2.1 Page caching
+
+The integration mobile detection with XF2.1+ full-page caching, add to the config.php this;
+```php
+$config['pageCache']['onSetup'] = function (\XF\PageCache $pageCache) {
+    $pageCache->setCacheIdGenerator(function(\XF\Http\Request $request) {
+        return \SV\BrowserDetection\CacheHelper::getPageCacheId($request);
+    });
+};
+```
+
+## licence
+
 Add-on licence file: LICENSE.md
 Mobile-Detect licence file: MobileDetectLicense.md
---
+++ upload/src/addons/SV/BrowserDetection/CacheHelper.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace SV\BrowserDetection;
+
+class CacheHelper
+{
+    public static function getPageCacheId(\XF\Http\Request $request)
+    {
+        $options = \XF::options();
+
+        $styleId = intval($request->getCookie('style_id', 0));
+        if (!$styleId)
+        {
+            $styleId = $options->defaultStyleId;
+        }
+        $languageId = intval($request->getCookie('language_id', 0));
+        if (!$languageId)
+        {
+            $languageId = $options->defaultLanguageId;
+        }
+
+        $uri = $request->getFullRequestUri();
+        $uri = preg_replace('#(\?|&)_debug=[^&]*#', '', $uri);
+
+        $isMobile = Listener::getMobileDetection()->isMobile() ? "_m1" : "_m0";
+
+        return 'page_' . sha1($uri) . '_' . strlen($uri) . "_s{$styleId}_l{$languageId}_v" . \XF\PageCache::CACHE_VERSION . $isMobile;
+    }
+}
