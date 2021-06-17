29098259ea4d48788c4f305bbf8006f62bbf003b
Xon <tazipper1@hotmail.com>
2021-06-17 09:31:43 +0800

    Prefix some global functions

--- upload/src/addons/SV/BrowserDetection/CacheHelper.php
+++ upload/src/addons/SV/BrowserDetection/CacheHelper.php
@@ -8,12 +8,12 @@ class CacheHelper
     {
         $options = \XF::options();
 
-        $styleId = intval($request->getCookie('style_id', 0));
+        $styleId = \intval($request->getCookie('style_id', 0));
         if (!$styleId)
         {
             $styleId = $options->defaultStyleId;
         }
-        $languageId = intval($request->getCookie('language_id', 0));
+        $languageId = \intval($request->getCookie('language_id', 0));
         if (!$languageId)
         {
             $languageId = $options->defaultLanguageId;
@@ -21,11 +21,11 @@ class CacheHelper
 
         $uri = $request->getFullRequestUri();
         /** @noinspection RegExpSingleCharAlternation */
-        $uri = preg_replace('#(\?|&)_debug=[^&]*#', '', $uri);
+        $uri = \preg_replace('#(\?|&)_debug=[^&]*#', '', $uri);
 
         $mobileDetect = Listener::getMobileDetection();
         $isMobile = $mobileDetect && $mobileDetect->isMobile() ? "_m1" : "_m0";
 
-        return 'page_' . sha1($uri) . '_' . strlen($uri) . "_s{$styleId}_l{$languageId}_v" . \XF\PageCache::CACHE_VERSION . $isMobile;
+        return 'page_' . \sha1($uri) . '_' . \strlen($uri) . "_s{$styleId}_l{$languageId}_v" . \XF\PageCache::CACHE_VERSION . $isMobile;
     }
 }
